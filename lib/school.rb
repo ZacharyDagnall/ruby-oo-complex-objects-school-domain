dc72b638892bd97e60202ca71ea903fc8535decc
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-12 13:13:44 -0500

    commit tree 9cfc804257727d087b534359539e16aaa7adce90

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+    attr_accessor :name
+    attr_accessor :roster
+
+    def initialize(given_name)
+        @name = given_name
+        @roster = {}
+    end
+
+    def add_student(st_name, st_grade)
+        if !@roster[st_grade]
+            @roster[st_grade] = []
+        end
+        @roster[st_grade] << st_name
+    end
+
+    def grade(grade_num)
+        @roster[grade_num]
+    end
+
+    def sort
+        @roster.each do |grade,students|
+            students.sort!
+        end
+        @roster
+    end
+
+end
